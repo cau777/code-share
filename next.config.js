f50f6db03f8b0c6f3db26da7a332ae85d7c838ec
Cauã Rinaldi <cauarinaldi451@gmail.com>
2022-09-07 10:24:36 -0300

    Add Dockerfile

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    images: {
-      unoptimized: true
-    }
-  }
+  output: "standalone"
 }
 
 module.exports = nextConfig
