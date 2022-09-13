b482e2cfa3e55b3e0d852290cc5600ff7cc6b1cc
Cauã Rinaldi <cauarinaldi451@gmail.com>
2022-09-13 18:24:21 -0300

    Translate a message

--- client/components/AssertProfileComplete.tsx
+++ client/components/AssertProfileComplete.tsx
@@ -1,13 +1,16 @@
 import {FC, PropsWithChildren, useContext} from "react";
 import {AuthContext} from "./AuthContext";
 import {useRouter} from "next/router";
+import {useTranslation} from "next-i18next";
 
 const AssertProfileComplete: FC<PropsWithChildren> = (props) => {
     let context = useContext(AuthContext);
     let router = useRouter();
+    let {t} = useTranslation();
+    
     if (context.loggedIn && !context.completedProfile) {
         router.push("/firstlogin").then();
-        return (<h3>You must complete your profile to access this page</h3>); // TODO: translate
+        return (<h3>{t("errorMustCompleteProfile")}</h3>);
     }
     return (<>{props.children}</>);
 }
