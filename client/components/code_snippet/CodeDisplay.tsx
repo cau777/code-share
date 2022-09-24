e76736ef706243c01a52d825bd2ef5c459557215
Cauã Rinaldi <cauarinaldi451@gmail.com>
2022-09-24 10:35:34 -0300

    Fix bugs in CodeDisplay.tsx

--- client/components/code_snippet/CodeDisplay.tsx
+++ client/components/code_snippet/CodeDisplay.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from "react";
+import {FC, useEffect, useState} from "react";
 import {LanguageOptions} from "../../src/code/code_editor_types";
 import {highlightText} from "../../src/code/syntax_highlight";
 import CodeEditorLine from "./CodeLine";
@@ -11,25 +11,21 @@ type Props = {
     language: LanguageOptions;
 }
 
-type State = {
-    visible: boolean;
-    highlight: [string, Uint8Array][];
-}
 
 const CodeDisplay: FC<Props> = (props) => {
     const options = props.language;
     const height = 23 * countOccurrences(props.text, "\n");
-    const [state, setState] = useState<State>({visible: false, highlight: []});
+    const [visible, setVisible] = useState(false);
+    const [state, setState] = useState< [string, Uint8Array][]>([]);
     
     const {ref} = useInView({
         threshold: 0.001,
-        onChange: visible => {
-            if (state.highlight)
-                setState({visible, highlight: highlightText(props.text, options)});
-            else
-                setState(s => ({...s, visible}));
-        }
+        onChange: setVisible
     });
+    
+    useEffect(() => {
+        setState(highlightText(props.text, options));
+    }, [options, props.text]);
     
     return (
         <table ref={ref} className={"w-full whitespace-pre monospace code"} style={{height}}>
@@ -37,8 +33,8 @@ const CodeDisplay: FC<Props> = (props) => {
                 <col className={"bg-back-1"}/>
             </colgroup>
             <tbody>
-            {state.visible ?
-                state.highlight.map(([line, highlight], index) =>
+            {visible ?
+                state.map(([line, highlight], index) =>
                     <CodeEditorLine key={"line " + index} selected={props.selected === index} text={line}
                                     highlights={highlight}/>)
                 : (<></>)}
